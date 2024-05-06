124c6963d4a3438ffbb171a03939edee2075d81d
decker12w <josemaia.comp@gmail.com>
2024-05-06 00:37:54 -0300

    final_correc

--- shell.c
+++ shell.c
@@ -1,3 +1,25 @@
+/*
+Autor: José Maia de Oliveira
+Data: 05/05/2024
+RA: 823395
+
+Estruturas implementadas:
+
+a. $ prog    X
+b. $ prog &   X             .
+c. $ prog parâmetros X
+d. $ prog parâmetros & X
+e. $ prog [parâmetros] > arquivo X
+f. $ prog1 | prog 2  Metáde da implementação. Ele so funciona para 2 comandos
+encadeados, não para uma cadeia de comandos
+
+Comandos implementados: jobs, fg, bg, cd, exit
+
+Obs: Caso haja algum problema com a exibição do código, pode acessa-lo nesse
+repositório do git:
+
+*/
+
 #include <fcntl.h>
 #include <pwd.h>
 #include <stdio.h>
@@ -6,12 +28,12 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-// Contantes
+// Constantes
 #define MAX_CMD_LEN 128 // Tamanho máximo do comando
 #define MAX_ARGS 50     // Número máximo de argumentos
 
-// Estrutura de dados para armazenar informações sobre processos
-// A estrutura funciona como uma lista encadeada
+// Estrutura de dados para armazenar informações sobre um processo
+// Ela é uma lista encadeada de processos
 typedef struct process {
   struct process *next;
   char *comando;
@@ -22,10 +44,10 @@ typedef struct process {
   int status;
 } process;
 
-// Variável global para armazenar processos
+// Inicialização da lista de processos
 process *processes = NULL;
 
-// Protótipos
+// Protótipos de funções
 int comandosCriados(char *[]);
 int comandoCd(char *[]);
 int comandoExit();
@@ -34,15 +56,22 @@ int comandoFg(char *[]);
 int comandoBg(char *[]);
 void limparProcessos();
 void impressãoPrompt();
-process *CriadorProcesso(char *, char **, pid_t);
+process *criarProcesso(char *, char **, pid_t);
+void executarComandoComPipe(char *cmd);
 
-// Signal handler para SIGCHLD
-//
+// Função de tratamento de sinal SIGCHLD para atualizar informações sobre os
+// processos filhos, desalocando os recursos utilizados, para que os processos
+// não virem um Zombies
 void sigchld_handler(int sig) {
   process *p = processes;
-  while (p != NULL) {
-    if (waitpid(p->pid, &(p->status), WNOHANG | WUNTRACED) > 0) {
 
+  // Percorre a lista de processos para verificar se algum processo foi:
+  // terminado, parado, ou finalizado e imprime o estado do processo
+  while (p != NULL) {
+    // O WNOHANG faz com que a função waitpid não bloqueie o processo pai e o
+    // WUNTRACED faz com que a função waitpid retorne se o processo filho foi
+    // parado
+    if (waitpid(p->pid, &(p->status), WNOHANG | WUNTRACED) > 0) {
       if (WIFEXITED(p->status)) {
         printf("Processo %d terminou com status %d\n", p->pid,
                WEXITSTATUS(p->status));
@@ -64,123 +93,137 @@ void sigchld_handler(int sig) {
 int main(int argc, char *argv[]) {
 
   char cmd[MAX_CMD_LEN];
-  int pid;
+  int pid1, pid2;
   int status;
 
+  // Definição do sigaction para tratar o sinal SIGCHLD
   struct sigaction sa;
   sa.sa_handler = &sigchld_handler;
   sigemptyset(&sa.sa_mask);
   sa.sa_flags = 0;
+
   if (sigaction(SIGCHLD, &sa, 0) == -1) {
     perror(0);
     exit(1);
   }
 
+  // Loop principal do shell
   while (1) {
 
+    // Função para limpar a memoria alocada para os processos finalizados
     limparProcessos();
+
+    // Função para imprimir o prompt do shell: user@hostname:cwd$
     impressãoPrompt();
+
+    // Leitura do comando do usuário
     if (fgets(cmd, MAX_CMD_LEN, stdin) == NULL) {
       break;
     }
+
+    // Verifica se o comando contém um caractere de nova linha decorrente do
+    // enter e o remove
     cmd[strcspn(cmd, "\n")] = '\0';
 
+    // Verifica se o comando contém um pipe e executa uma função exclusiva para
+    // executar comandos com pipe
+    if (strchr(cmd, '|') != NULL) {
+      executarComandoComPipe(cmd);
+      continue;
+    }
+
+    // Separa o comando em argumentos com os tokens do strtok e armazena em um
+    // vetor de strings
     int i = 0;
     argv[i] = strtok(cmd, " ");
 
     while (argv[i] != NULL) {
+
       argv[++i] = strtok(NULL, " ");
     }
 
+    // Comando exit
     if (strcmp(argv[0], "exit") == 0) {
       comandoExit();
     }
 
+    // Verifica se o comando é um dos comandos internos implementados e os
+    // executa
     if (comandosCriados(argv) >= 0) {
       continue;
     }
 
+    // Verifica se o comando contém um simbolo de redirecionamento
     int fd = -1;
     for (int j = 0; j < i; j++) {
       if (strcmp(argv[j], ">") == 0) {
+
+        // Abre o arquivo de log para escrita
         fd = open(argv[j + 1], O_CREAT | O_WRONLY | O_APPEND, 0666);
         if (fd < 1) {
           perror("Erro ao abrir o arquivo de log");
           exit(EXIT_FAILURE);
         }
-        argv[j] = NULL;
+        argv[j] = NULL; // Remove o redirecionamento do argv
         break;
       }
     }
 
-    int pipeActive = 0;
-    int pipeLocation = -1;
-    int pipeDescritor[2];
-
-    for (int q = 0; q < i; q++) {
-      if (strcmp(argv[q], "|") == 0) {
-        pipeLocation = q;
-        if (pipe(pipeDescritor) < 0) {
-          printf("Erro ao criar o pipe");
-        };
-        argv[q] = NULL;
-        pipeActive = 1;
-        break;
-      }
-    }
-
+    // Verifica se o comando deve ser executado em segundo plano
     int background = 0;
     if (i > 0 && strcmp(argv[i - 1], "&") == 0) {
       background = 1;
       argv[i - 1] = NULL;
     }
 
-    pid = fork();
-    if (pid < 0) {
+    // Cria um processo filho para executar o comando
+    pid1 = fork();
+    if (pid1 < 0) {
       perror("Erro ao criar processo filho\n");
-    } else if (pid == 0) {
+    } else if (pid1 == 0) {
+
       setpgid(0, 0);
 
-      if (pipeActive) {
-        dup2(pipeDescritor[1], STDOUT_FILENO);
-        close(pipeDescritor[0]);
-        close(pipeDescritor[1]);
-      }
-
+      // Verifica se o comando deve ser redirecionado para um arquivo
       if (fd != -1) {
         dup2(fd, STDOUT_FILENO); // Redireciona a saída padrão para o arquivo
         close(fd);
       }
+
+      // Sobrepoem a imagem do binário especificado pelo comando no processo
+      // filho
       if ((execvp(argv[0], argv) == -1)) {
         perror("Erro ao executar o comando");
       }
       exit(EXIT_FAILURE);
+
     } else {
-      if (pipeActive) {
-        pid = fork();
-        if (pid < 0) {
-          perror("Erro ao criar processo filho\n");
-        } else if (pid == 0) {
-          setpgid(0, 0);
-          dup2(pipeDescritor[0], STDIN_FILENO);
-          close(pipeDescritor[0]);
-          close(pipeDescritor[1]);
-          if ((execvp(argv[pipeLocation + 1], argv + pipeLocation + 1) == -1)) {
-            perror("Erro ao executar o comando");
-          }
-          exit(EXIT_FAILURE);
-        }
-      }
 
-      process *p = CriadorProcesso(argv[0], argv, pid);
-      processes = p;
+      // Cria um processo para armazenar informações sobre o processo filho
+      process *p = criarProcesso(cmd, argv, pid1);
 
+      // Verifica se está em segundo plano e aguarda o término do processo
       if (!background) {
-        int pid_filho = wait(&status);
+        int pid_filho = waitpid(pid1, &status, WUNTRACED);
+
+        // Verifica o status do processo filho
         if (WIFEXITED(status)) {
           p->completed = 1;
           p->status = WEXITSTATUS(status);
         }
+        if (WIFSIGNALED(status)) {
+          p->completed = 1;
+          p->status = WTERMSIG(status);
+        }
+        if (WIFSTOPPED(status)) {
+          p->stopped = 1;
+          p->status = WSTOPSIG(status);
+        }
+      } else {
+
+        // Imprime mensagem informando que o processo está em execução em
+        // segundo plano
+        printf("Processo %d em execução em segundo plano\n", pid1);
       }
     }
   }
@@ -188,6 +231,7 @@ int main(int argc, char *argv[]) {
   return EXIT_SUCCESS;
 }
 
+// Função para verificar se o comando é um dos comandos internos implementados e
 int comandosCriados(char *argv[]) {
   if (strcmp(argv[0], "cd") == 0) {
     return comandoCd(argv);
@@ -201,9 +245,10 @@ int comandosCriados(char *argv[]) {
   return -1;
 }
 
-process *CriadorProcesso(char *comando, char **argv, pid_t pid) {
+// Função para criar um processo e armazenar informações sobre ele
+process *criarProcesso(char *comando, char **argv, pid_t pid) {
   process *p = malloc(sizeof(process));
-  p->comando = strdup(comando);
+  p->comando = strdup(argv[0]);
   p->argv = argv;
   p->pid = pid;
   p->completed = 0;
@@ -214,11 +259,13 @@ process *CriadorProcesso(char *comando, char **argv, pid_t pid) {
   return p;
 }
 
+// Função para executar o comando cd
 int comandoCd(char *argv[]) {
   if (argv[1] == NULL) {
     fprintf(stderr, "Esperado argumento para \"cd\"\n");
     return -1;
   } else {
+    // Se o comando cd for executado com sucesso, muda o diretório atual
     if (chdir(argv[1]) != 0) {
       perror("cd falhou");
       return -1;
@@ -227,13 +274,18 @@ int comandoCd(char *argv[]) {
   }
 }
 
+// Função para executar o comando exit
 int comandoExit() {
   exit(EXIT_SUCCESS);
   return 0;
 }
 
+// Função para executar o comando jobs listando os processos em execução
 int comandoJobs() {
   process *p = processes;
+
+  // Percorre a lista de processos e imprime o estado de cada processo se foi
+  // concluído, parado ou está em execução
   while (p != NULL) {
     printf("%d: %s", p->pid, p->comando);
     if (p->completed) {
@@ -248,6 +300,8 @@ int comandoJobs() {
   }
   return 0;
 }
+
+// Função para executar o comando fg
 int comandoFg(char *argv[]) {
   if (argv[1] == NULL) {
     fprintf(stderr, "Esperado argumento para \"fg\"\n");
@@ -255,12 +309,19 @@ int comandoFg(char *argv[]) {
   } else {
     pid_t pid = atoi(argv[1]);
     process *p = processes;
+
+    // Percorre a lista de processos para verificar se o processo está parado e
+    // envia um sinal para continuar a execução do processo caso o processo
+    // esteja parado
     while (p != NULL) {
       if (p->pid == pid) {
         if (p->stopped) {
           kill(pid, SIGCONT);
           p->stopped = 0;
         }
+
+        // Muda o grupo de processos para que o processo filho possa receber a
+        // entrada padrão do terminal
         tcsetpgrp(STDIN_FILENO, p->pid);
         waitpid(pid, &(p->status), WUNTRACED);
         if (WIFSTOPPED(p->status)) {
@@ -268,6 +329,8 @@ int comandoFg(char *argv[]) {
         } else {
           p->completed = 1;
         }
+
+        // Restaura o grupo de processos para o shell
         tcsetpgrp(STDIN_FILENO, getpgrp());
         return 1;
       }
@@ -278,6 +341,7 @@ int comandoFg(char *argv[]) {
   }
 }
 
+// Função para executar o comando bg
 int comandoBg(char *argv[]) {
   if (argv[1] == NULL) {
     fprintf(stderr, "Esperado argumento para \"bg\"\n");
@@ -285,6 +349,9 @@ int comandoBg(char *argv[]) {
   } else {
     pid_t pid = atoi(argv[1]);
     process *p = processes;
+
+    // Percorre a lista de processos para verificar se o processo está parado e
+    // envia um sinal para continuar a execução caso o processo esteja parado
     while (p != NULL) {
       if (p->pid == pid) {
         if (p->stopped) {
@@ -300,6 +367,7 @@ int comandoBg(char *argv[]) {
   }
 }
 
+// Desaloca a memória alocada para os processos finalizados
 void limparProcessos() {
   process *p = processes;
   process *anterior = NULL;
@@ -321,6 +389,7 @@ void limparProcessos() {
   }
 }
 
+// Função para imprimir o prompt do shell
 void impressãoPrompt() {
 
   struct passwd *pw;
@@ -334,5 +403,54 @@ void impressãoPrompt() {
   gethostname(hostname, 1024);
   getcwd(cwd, 1024);
 
-  printf("\033[36m%s@%s:%s $ \033[0m", user, hostname, cwd);
+  printf("\033[36m%s@%s:%s$ \033[0m", user, hostname, cwd);
+}
+
+// Função para executar comandos com pipe
+// Basicamente ele separa o comando em 2 comandos e executa cada um deles
+// criando um processo filho. Eles se comunicam atráves de um canal
+// unidirecional (pipe) que repassa a saída do primeiro comando para a entrada
+// do segundo comando.
+void executarComandoComPipe(char *cmd) {
+  char *cmd1 = strtok(cmd, "|");
+  char *cmd2 = strtok(NULL, "|");
+
+  char *argv1[MAX_ARGS];
+  char *argv2[MAX_ARGS];
+
+  int i = 0;
+  argv1[i] = strtok(cmd1, " ");
+  while (argv1[i] != NULL) {
+    argv1[++i] = strtok(NULL, " ");
+  }
+
+  i = 0;
+  argv2[i] = strtok(cmd2, " ");
+  while (argv2[i] != NULL) {
+    argv2[++i] = strtok(NULL, " ");
+  }
+
+  int pipefd[2];
+  pipe(pipefd);
+
+  pid_t pid1 = fork();
+  if (pid1 == 0) {
+    close(pipefd[0]);
+    dup2(pipefd[1], STDOUT_FILENO);
+    execvp(argv1[0], argv1);
+    exit(EXIT_FAILURE);
+  }
+
+  pid_t pid2 = fork();
+  if (pid2 == 0) {
+    close(pipefd[1]);
+    dup2(pipefd[0], STDIN_FILENO);
+    execvp(argv2[0], argv2);
+    exit(EXIT_FAILURE);
+  }
+
+  close(pipefd[0]);
+  close(pipefd[1]);
+  waitpid(pid1, NULL, 0);
+  waitpid(pid2, NULL, 0);
 }
